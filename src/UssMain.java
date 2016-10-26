61c32bc44f1e13ef00775bab2568d66c8a133288
Toomas Koost <toomas@wizibe.com>
2016-10-26 13:36:31 +0300

    tegin koodi ilusamaks ja lühemaks

--- src/UssMain.java
+++ src/UssMain.java
@@ -17,10 +17,10 @@ import java.util.TimerTask;
  * @version 23/10/16
  */
 public class UssMain extends Application  {
-    private boolean up;//kui up=TRUE siis uss liigub üles
-    private boolean left;//kui left=TRUE siis uss liigub vasakule
-    private boolean down;//...
-    private boolean right;//...
+    private boolean up = false;//kui up=TRUE siis uss liigub üles
+    private boolean left = false;//kui left=TRUE siis uss liigub vasakule
+    private boolean down = true;//...
+    private boolean right = false;//...
     private Circle ussike;//Ussike koosneb ringikujulistest lülidest
     private int x;//ussi X kordinaat
     private int y;//ussi Y kordinaat
@@ -102,175 +102,130 @@ public class UssMain extends Application  {
 
             }
         }, 1, 500);
-
-
-        //kohe alguses liigub alla
-        up = false;
-        down = true;
-        left = false;
-        right = false;
-
-
+        
         // jälgib nuppude liigutusi
         ussike.setOnKeyReleased(
                 e -> {
                     if (up) {
                         switch (e.getCode()) {//kui liigub üles, siis saab pöörata ainult paremale ja vasakule
                             case LEFT:
-                                up = false;
-                                down = false;
-                                right = false;
+                                allDirectionsFalse();
                                 left = true;
                                 break;
                             case RIGHT:
-                                up = false;
-                                left = false;
-                                down = false;
+                                allDirectionsFalse();
                                 right = true;
                         }
                     }
                     if (down) {
                         switch (e.getCode()) {//kui liigub alla siis saab pöörata ainult paremale ja vasakule
                             case LEFT:
-                                up = false;
-                                down = false;
-                                right = false;
+                                allDirectionsFalse();
                                 left = true;
                                 break;
                             case RIGHT:
-                                up = false;
-                                left = false;
-                                down = false;
+                                allDirectionsFalse();
                                 right = true;
                         }
                     }
                     if (right) {
                         switch (e.getCode()) {//kui liigub paremale, siis saab pöörata üles ja alla
                             case UP:
-                                left = false;
-                                down = false;
-                                right = false;
+                                allDirectionsFalse();
                                 up = true;
                                 break;
                             case DOWN:
-                                up = false;
-                                left = false;
-                                right = false;
+                                allDirectionsFalse();
                                 down = true;
                         }
                     }
                     if (left) {
                         switch (e.getCode()) {//kui liigub vasakule, siis saab pöörata üles ja alla
                             case UP:
-                                left = false;
-                                down = false;
-                                right = false;
+                                allDirectionsFalse();
                                 up = true;
                                 break;
                             case DOWN:
-                                up = false;
-                                left = false;
-                                right = false;
+                                allDirectionsFalse();
                                 down = true;
                         }
                     }
                 }
         );
     }
-
-
     private void ussikeliigub(){//kõik mida ussike peab tegema kui ta liigub mingis suunas
 
         if(up){//ussike liigub üles
-            ussike.setCenterY(y);
-            ussike.setCenterX(x);
+            setCenter();
             y = y-10;//iga kord kui 0,5 sek möödas, siis muudab ussi asukohta ja triggerib uuesti setCenterX ja Y
             if(ussike.getCenterX() == nomX && ussike.getCenterY() == nomY){//kui ussike läheb samale asukohale kus on nomnom, siis tee järgmist
                 counterInt++;//suurenda skoori
-                nomX = (int) ((Math.round(((Math.random() * 500) + 5)) / 10) * 10);//niimoodi kalkuleerib arvu vahemikus 10-500
-                nomY = (int) ((Math.round(((Math.random() * 500) + 5)) / 10) * 10);//niimoodi kalkuleerib arvu vahemikus 10-500
-                while (nomX >=490 || nomX <= 10) {//jälgib, et arv jääks mänguruumi alasse
-                    nomX = (int) ((Math.round(((Math.random() * 500) + 5)) / 10) * 10);
-                }
-                while(nomY >=490 || nomY <= 50) {//jälgib, et arv jääks mänguruumi alasse
-                    nomY = (int) ((Math.round(((Math.random() * 500) + 5)) / 10) * 10);
-                }
-                nomnom.setCenterX(nomX);//võta nomnomile uus nomX
-                nomnom.setCenterY(nomY);//võta nomnomile uus nomY
+                makenomnom();
             }
             if(y<=40){//kui lähed alast välja
-                gameOver.setFont(Font.font(30));//gameover yeee
-                timer.cancel();//mäng läbi, enam pole animatsiooni
+                gameOverJEE();
             }
         }
         if(left){
-            ussike.setCenterY(y);
-            ussike.setCenterX(x);
+            setCenter();
             x = x-10;
             if(ussike.getCenterX() == nomX && ussike.getCenterY() == nomY){
                 counterInt++;
-                nomX = (int) ((Math.round(((Math.random() * 500) + 5)) / 10) * 10);
-                nomY = (int) ((Math.round(((Math.random() * 500) + 5)) / 10) * 10);
-                while (nomX >=490 || nomX <= 10) {
-                    nomX = (int) ((Math.round(((Math.random() * 500) + 5)) / 10) * 10);
-                }
-                while(nomY >=490 || nomY <= 50) {
-                    nomY = (int) ((Math.round(((Math.random() * 500) + 5)) / 10) * 10);
-                }
-                nomnom.setCenterX(nomX);
-                nomnom.setCenterY(nomY);
+                makenomnom();
             }
             if(x<=5){
-                gameOver.setFont(Font.font(30));
-                timer.cancel();
+                gameOverJEE();
             }
         }
         if(right){
-            ussike.setCenterY(y);
-            ussike.setCenterX(x);
+            setCenter();
             x= x+10;
             if(ussike.getCenterX() == nomX && ussike.getCenterY() == nomY){
                 counterInt++;
-                nomX = (int) ((Math.round(((Math.random() * 500) + 5)) / 10) * 10);
-                nomY = (int) ((Math.round(((Math.random() * 500) + 5)) / 10) * 10);
-                while (nomX >=490 || nomX <= 10) {
-                    nomX = (int) ((Math.round(((Math.random() * 500) + 5)) / 10) * 10);
-                }
-                while(nomY >=490 || nomY <= 50) {
-                    nomY = (int) ((Math.round(((Math.random() * 500) + 5)) / 10) * 10);
-                }
-                nomnom.setCenterX(nomX);
-                nomnom.setCenterY(nomY);
+                makenomnom();
             }
             if(x>=495){
-                gameOver.setFont(Font.font(30));
-                timer.cancel();
+                gameOverJEE();
             }
 
         }
 
         if(down){
-            ussike.setCenterY(y);
-            ussike.setCenterX(x);
+            setCenter();
             y = y+10;
             if(ussike.getCenterX() == nomX && ussike.getCenterY() == nomY){
                 counterInt++;
-                nomX = (int) ((Math.round(((Math.random() * 500) + 5)) / 10) * 10);
-                nomY = (int) ((Math.round(((Math.random() * 500) + 5)) / 10) * 10);
-                while (nomX >=490 || nomX <= 10) {
-                    nomX = (int) ((Math.round(((Math.random() * 500) + 5)) / 10) * 10);
-                }
-                while(nomY >=490 || nomY <= 50) {
-                    nomY = (int) ((Math.round(((Math.random() * 500) + 5)) / 10) * 10);
-                }
-                nomnom.setCenterX(nomX);
-                nomnom.setCenterY(nomY);
+                makenomnom();
             }
             if(y>=495){
-                gameOver.setFont(Font.font(30));
-                timer.cancel();
+                gameOverJEE();
             }
         }
     }
-
+    private void makenomnom(){//lühendada koodi
+        nomX = (int) ((Math.round(((Math.random() * 500) + 5)) / 10) * 10);//niimoodi kalkuleerib arvu vahemikus 10-500
+        nomY = (int) ((Math.round(((Math.random() * 500) + 5)) / 10) * 10);//niimoodi kalkuleerib arvu vahemikus 10-500
+        while (nomX >=490 || nomX <= 10) {//jälgib, et arv jääks mänguruumi alasse
+            nomX = (int) ((Math.round(((Math.random() * 500) + 5)) / 10) * 10);
+        }
+        while(nomY >=490 || nomY <= 50) {//jälgib, et arv jääks mänguruumi alasse
+            nomY = (int) ((Math.round(((Math.random() * 500) + 5)) / 10) * 10);
+        }
+        nomnom.setCenterX(nomX);//võta nomnomile uus nomX
+        nomnom.setCenterY(nomY);//võta nomnomile uus nomY
+    }
+    private void setCenter(){//lühendada koodi
+        ussike.setCenterY(y);
+        ussike.setCenterX(x);
+    }
+    private void gameOverJEE(){//lühendada koodi
+        gameOver.setFont(Font.font(30));//gameover yeee
+        timer.cancel();//mäng läbi, enam pole animatsiooni
+    }
+    private void allDirectionsFalse(){//ükskõik mis noolt vajutada, kõigepealt paneb kõik falseks ja kohe peale seda muudab ühe suuna trueks
+        up = false;
+        down = false;
+        right = false;
+        left = false;
+    }
 }
